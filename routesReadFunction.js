514cd3f119124fc5a7f192fa6aa20906c5976174
kalinag123 <165840154+kalinag123@users.noreply.github.com>
2024-05-30 18:00:47 -0700

    Improving read function logic

--- routesReadFunction.js
+++ routesReadFunction.js
@@ -52,26 +52,49 @@ carousel.innerHTML += carouselButtons;
 // Now that the carousel exists, go through the routes data and populate carousel slides with cards.
 let rows = carousel.getElementsByClassName("row");
 
-let dataIndex = 0;
-for(let i = 0; i < rows.length; ++ i) {
-    for(let j = 0; j < cardsPerSlide; ++j) {
-        let current = data[dataIndex];
-        let card = `
-            <div class="col d-flex justify-content-center">
-                <div class="card" style="width: 24rem;">
-                    <img src=${current.imgURL} class="card-img-top" alt="Map image of ${current.title}">
-                    <div class="card-body">
-                        <h5 class="card-title">${current.title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${current.datetime}</h6>
-                        <p class="card-text">${current.text}</p>
-                        <a href="#" class="btn btn-primary" onclick="notImplemented()">Book this ride</a>
-                    </div>
+let rowsIndex = 0;
+for(let i = 0; i < data.length; ++ i) {
+    let card = `
+        <div class="col d-flex justify-content-center">
+            <div class="card" style="width: 24rem;">
+                <img src=${current.imgURL} class="card-img-top" alt="Map image of ${current.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${current.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${current.datetime}</h6>
+                    <p class="card-text">${current.text}</p>
+                    <a href="#" class="btn btn-primary" onclick="notImplemented()">Book this ride</a>
                 </div>
-            </div>  
-        `;
+            </div>
+        </div>  
+    `;
 
-        rows[i].innerHTML += card;
-
-        ++dataIndex;
+    if(i % 3 == 0 && i != 0) {
+        ++rowsIndex;
     }
+
+    rows[rowsIndex].innerHTML += card;
 }
+
+// let dataIndex = 0;
+// for(let i = 0; i < rows.length; ++ i) {
+//     for(let j = 0; j < cardsPerSlide; ++j) {
+//         let current = data[dataIndex];
+//         let card = `
+//             <div class="col d-flex justify-content-center">
+//                 <div class="card" style="width: 24rem;">
+//                     <img src=${current.imgURL} class="card-img-top" alt="Map image of ${current.title}">
+//                     <div class="card-body">
+//                         <h5 class="card-title">${current.title}</h5>
+//                         <h6 class="card-subtitle mb-2 text-muted">${current.datetime}</h6>
+//                         <p class="card-text">${current.text}</p>
+//                         <a href="#" class="btn btn-primary" onclick="notImplemented()">Book this ride</a>
+//                     </div>
+//                 </div>
+//             </div>  
+//         `;
+
+//         rows[i].innerHTML += card;
+
+//         ++dataIndex;
+//     }
+// }
